9e31d508436dcb80e0a13e95a2a80ccdca031c01
Willy Cordon <willy@broobe.com>
2020-10-30 09:52:50 -0300

    build

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { GifExpertApp } from './GifExpertApp';
-
 import './index.css';
+import { GifExpertApp } from './GifExpertApp';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 
 ReactDOM.render(
-  <GifExpertApp />,
-  document.getElementById('root')
+    <React.StrictMode>
+        <GifExpertApp />,
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 
